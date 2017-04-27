d80eeacb9f50b23dc4da1861083dbcacb382b0da
nnattawat <armmer1@gmail.com>
2017-04-27 16:44:10 +1000

    Init

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,30 @@
+let React = require('react');
+let { Link, IndexLink } = require('react-router');
+
+let Navigation = () => {
+  return (
+    <div className='top-bar'>
+      <div className='top-bar-left'>
+        <ul className='menu'>
+          <li className='menu-text'>
+            Timer
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+            <IndexLink to="/" activeClassName="active-link">Count Down</IndexLink>
+          </li>
+        </ul>
+      </div>
+      <div className='top-bar-right'>
+        <ul className='menu'>
+          <li className='menu-text'>
+            Created by nnattawat
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+
+};
+
+module.exports = Navigation;
